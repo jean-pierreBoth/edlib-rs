3872f78d2f807c5e41d6a599d6649ddf6442ef89
jean-pierre both <jeanpierre.both@gmail.com>
2020-09-07 14:23:37 +0200

    build.rs must write only in OUT_DIR

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::io::prelude::*;
 
 // We have path to edlib, generate wrapper.h with  path to correct include 
-fn write_wrapper(edlibpath : &PathBuf) {
+fn write_wrapper(edlibpath : &PathBuf, wrapper_path : &PathBuf) {
     let mut inclpath = edlibpath.clone();
     inclpath.push("edlib");
     inclpath.push("include");
@@ -16,7 +16,7 @@ fn write_wrapper(edlibpath : &PathBuf) {
     incl.push_str(& inclpath.to_str().unwrap());
     incl.push_str(">");
     // now we write file wrapper.h
-    let mut f = File::create("wrapper.h").unwrap();
+    let mut f = File::create(wrapper_path).unwrap();
     f.write_all(incl.as_bytes()).expect("cannot write wrapper.h");
 }
 
@@ -26,8 +26,10 @@ fn main() {
     let edlib_env = env::var("EDLIB_DIR");
     let edlib_src = edlib_env.expect("env variable EDLIB_DIR not set");
     let edlib_path = PathBuf::from(edlib_src);
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let wrapper_path = out_path.join("wrapper.h");
     //
-    write_wrapper(&edlib_path);
+    write_wrapper(&edlib_path, &wrapper_path);
     //
     let mut libdir = String::from("cargo:rustc-link-search=");
     libdir.push_str(edlib_path.to_str().unwrap());
@@ -42,7 +44,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("wrapper.h")
+        .header(wrapper_path.to_str().unwrap())
         //Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -52,7 +54,6 @@ fn main() {
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     let src_path = PathBuf::from("src");
     //
     println!("cargo:rerun-if-changed=binding.rs");
